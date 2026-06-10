21268b4b5a71b8536f58e093c3daa75fe19c6976
Gabryelx7 <gabrielmsd@live.com>
2026-06-09 22:57:40 -0300

    feat: install JBDC driver to spark

--- src/spark/session_builder.py
+++ src/spark/session_builder.py
@@ -10,7 +10,8 @@ def get_spark_session(app_name="SIEM_Lakehouse"):
     extra_packages = [
         "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0",
         "org.apache.hadoop:hadoop-aws:3.3.2",
-        "com.amazonaws:aws-java-sdk-bundle:1.12.262"
+        "com.amazonaws:aws-java-sdk-bundle:1.12.262",
+        "org.postgresql:postgresql:42.7.3"
     ]
 
     minio_user = os.getenv("MINIO_USER", "admin")
